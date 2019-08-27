489c503b643ddd645983611297132523d8ef04ea
Brendan Hogan <bhogan@mitre.org>
2019-08-27 09:00:41 -0400

    more tests

--- tests/testthat/test_utils.R
+++ tests/testthat/test_utils.R
@@ -28,4 +28,52 @@ test_that("parameter extraction works", {
   param <- get_cluster_param(configs, "param1", "scope2")
   expect_equal(class(param), "character")
   expect_equal(length(param), 1)
+  
+  # invalid scope with NULL default
+  param <- get_cluster_param(configs, "param1", "scope3")
+  expect_null(param)
+
+  # invalid scope with provided default
+  param <- get_cluster_param(configs, "param1", "scope3", default = FALSE)
+  expect_false(param)
+})
+
+test_that("is_valid_scope works as expected", {
+  with_mock(
+    is_cluster_known=function(x, stopifnot){return(TRUE)},
+    get_cluster_package_name=function(x){return("clusterconf")},
+    configs <- get_cluster_configs("abc")
+  )
+  
+  # warning is issued by default
+  expect_warning(clusterconf:::is_valid_scope(configs, "scope3"), 
+                 regexp = paste0("Cannot return configurations for scope: 'scope3'. ",
+                                 "Valid scopes are: 'scope1', 'scope2'. Returning NULL."))
+  
+  # opt out of warning
+  expect_false(clusterconf:::is_valid_scope(configs, "scope3", warn = FALSE))
+  
+  # normal behavior
+  expect_true(clusterconf:::is_valid_scope(configs, "scope1"))
+})
+
+
+test_that("get_java_dependencies_path works with provided cluster name", {
+  tmp_dir <- file.path("fake", "package", "dir")
+  
+  with_mock(
+    find.package=function(package){return(tmp_dir)},
+    expect_equal(get_java_dependencies_path("abc"), 
+                 file.path(tmp_dir, "inst", "java"))
+  )
+})
+
+test_that("get_java_dependencies_path works with provided package name", {
+  tmp_dir <- file.path("fake", "package", "dir")
+  
+  with_mock(
+    find.package=function(package){return(tmp_dir)},
+    expect_equal(get_java_dependencies_path(pkg_name = "clusterconf.abc"), 
+                 file.path(tmp_dir, "inst", "java"))
+  )
 })
